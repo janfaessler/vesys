444fd1bf39fcecd282b1898f5048ef6f627e6e2c
Jan Fässler <mail@janfaessler.ch>
2013-04-09 13:12:42 +0200

    complete rewrite and adding a xmlrpc version

--- RemoteBank/src/server/util/RequestHandler.java
+++
@@ -1,8 +0,0 @@
-package server.util;
-
-import java.io.IOException;
-
-public interface RequestHandler {
-	public void stop() throws IOException;
-	public void sendCommand(String command, String param) throws IOException;
-}
